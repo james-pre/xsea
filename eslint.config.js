8a5b78aff80986f090cd1ede986a857693d806c4
James Prevett <jp@jamespre.dev>
2024-11-28 23:59:33 -0600

    Added Windows/zip support, caching

--- eslint.config.js
+++ eslint.config.js
@@ -41,10 +41,11 @@ export default tseslint.config({
 		'@typescript-eslint/require-await': 'warn',
 		'@typescript-eslint/no-unsafe-return': 'off',
 		'@typescript-eslint/no-unsafe-assignment': 'off',
-		'@typescript-eslint/no-unsafe-member-access': 'warn',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
 		'@typescript-eslint/no-unsafe-argument': 'off',
 		'@typescript-eslint/no-redundant-type-constituents': 'warn',
 		'@typescript-eslint/no-unsafe-call': 'off',
 		'@typescript-eslint/no-unused-expressions': 'off',
+		'@typescript-eslint/only-throw-error': 'off',
 	},
 });
--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,11 @@
+#!/bin/env node
 import { execSync } from 'node:child_process';
 import * as fs from 'node:fs';
 import { dirname, join, parse } from 'node:path';
 import { parseArgs } from 'node:util';
 import { inject } from 'postject';
 import { extract } from 'tar';
+import AdmZip from 'adm-zip';
 
 const { values: options, positionals } = parseArgs({
 	options: {
@@ -13,6 +15,7 @@ const { values: options, positionals } = parseArgs({
 		output: { short: 'o', type: 'string' },
 		clean: { type: 'boolean', default: false },
 		node: { short: 'N', type: 'string', default: 'v' + process.versions.node },
+		platform: { short: 'P', type: 'string', multiple: true, default: [process.platform + '-' + process.arch] },
 	},
 	allowPositionals: true,
 });
@@ -29,7 +32,9 @@ Options:
     --quiet,-q              Hide non-error output
     --verbose,-w            Show all output
     --output, -o <prefix>   The output prefix
-	--tempd                 Temporary files directory, 
+    --clean                 Remove temporary files
+    --node,-N <version>     Specify the Node version
+    --platform, -P <plat>   Specify which platform(s) to build for
 `);
 	process.exit(0);
 }
@@ -39,21 +44,31 @@ if (options.verbose && options.quiet) {
 	process.exit(1);
 }
 
+if (options.clean) {
+	_log('Removing temporary files...');
+	fs.rmSync(tempDir, { recursive: true, force: true });
+}
+
 if (positionals.length != 1) {
+	if (options.clean) process.exit(0);
 	console.error('Incorrect number of positional arguments, expected 1');
 	process.exit(1);
 }
 
-const prefix = options.output ?? parse(positionals[0]).name;
+const entryName = parse(positionals[0]).name;
 
-if (options.clean) {
-	_log('Removing temporary files...');
-	fs.rmSync(tempDir, { recursive: true });
+let prefix = options.output ?? entryName;
+
+if (/\w$/.test(prefix)) {
+	prefix += '-';
 }
-fs.mkdirSync(tempDir, { recursive: true });
 
-const configPath = join(tempDir, 'sea.json'),
-	blobPath = join(tempDir, 'server.blob');
+_log('Prefix:', prefix);
+
+fs.mkdirSync(join(tempDir, 'node'), { recursive: true });
+
+const configPath = join(tempDir, entryName + '.json'),
+	blobPath = join(tempDir, entryName + '.blob');
 
 fs.writeFileSync(
 	configPath,
@@ -69,6 +84,49 @@ const blob = fs.readFileSync(blobPath);
 
 fs.mkdirSync(prefix.endsWith('/') ? prefix : dirname(prefix), { recursive: true });
 
+async function getNode(archiveBase: string) {
+	const isWindows = archiveBase.startsWith('node-win');
+
+	const archiveFile = archiveBase + '.' + (isWindows ? 'zip' : 'tar.gz');
+	const archivePath = join(tempDir, archiveFile);
+	const execName = join(tempDir, archiveBase);
+
+	if (fs.existsSync(execName)) {
+		_log('Found existing:', archiveBase);
+		return;
+	}
+
+	if (fs.existsSync(archivePath)) {
+		_log('Found existing archive:', archiveFile);
+	} else {
+		try {
+			const url = `https://nodejs.org/dist/${options.node}/${archiveFile}`;
+			_log('Fetching:', url);
+			const response = await fetch(url);
+			fs.writeFileSync(archivePath, new Uint8Array(await response.arrayBuffer()));
+		} catch {
+			throw ['Failed to download:', archiveBase];
+		}
+	}
+
+	_log('Extracting:', archiveFile);
+	if (isWindows) {
+		const zip = new AdmZip(archivePath);
+		const data = zip.readFile(isWindows ? 'node.exe' : 'bin/node');
+		if (!data) {
+			throw ['Missing node executable:', archiveBase];
+		}
+		fs.writeFileSync(execName, data);
+	} else {
+		await extract({
+			file: archivePath,
+			gzip: true,
+			cwd: join(tempDir, 'node'),
+		});
+		fs.copyFileSync(join(tempDir, 'node', archiveBase, isWindows ? 'node.exe' : 'bin/node'), execName);
+	}
+}
+
 /**
  * Builds a SEA for a target (e.g. win-x64, linux-arm64)
  */
@@ -76,38 +134,26 @@ async function buildSEA(target: string) {
 	!options.quiet && console.log('Creating SEA for:', target);
 	const isWindows = target.startsWith('win');
 
-	const seaPath = join(prefix, isWindows ? target + '.exe' : target);
+	const seaPath = prefix + (isWindows ? target + '.exe' : target);
 
-	const archiveFile = `node-${options.node}-${target}.${isWindows ? 'zip' : 'tar.gz'}`;
-	const archivePath = join(tempDir, archiveFile);
+	const archiveBase = `node-${options.node}-${target}`;
+
 	try {
-		const url = `https://nodejs.org/dist/${options.node}/${archiveFile}`;
-		_log('Fetching:', url);
-		const response = await fetch(url);
-		fs.writeFileSync(archivePath, new Uint8Array(await response.arrayBuffer()));
-	} catch {
-		console.error(`Failed to download Node v${options.node} for ${target}`);
+		await getNode(archiveBase);
+	} catch (e: any) {
+		console.error(...(Array.isArray(e) ? e : [e]));
 		return;
 	}
 
-	const extractedDir = join(tempDir, target);
-	fs.mkdirSync(extractedDir, { recursive: true });
-
-	_log('Extracting:', archivePath);
-	if (isWindows) {
-	} else {
-		await extract({
-			file: archivePath,
-			gzip: true,
-			cwd: extractedDir,
-		});
-	}
-
 	fs.mkdirSync(dirname(seaPath), { recursive: true });
-	fs.copyFileSync(join(extractedDir, isWindows ? 'node.exe' : 'node'), seaPath);
+	fs.copyFileSync(join(tempDir, archiveBase), seaPath);
 	_log('Injecting:', seaPath);
-	inject(seaPath, 'NODE_SEA_BLOB', blob, {
+	await inject(seaPath, 'NODE_SEA_BLOB', blob, {
 		machoSegmentName: 'NODE_SEA',
 		sentinelFuse: 'NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
 	});
+}
+
+for (const target of options.platform) {
+	await buildSEA(target);
 }
